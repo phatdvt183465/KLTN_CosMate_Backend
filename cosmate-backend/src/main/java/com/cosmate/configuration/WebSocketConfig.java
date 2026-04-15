0bd518f3bbc805d59bee5bca0e339c62b52cc9c7
khoantkse183194@fpt.edu.vn <khoantkse183194@fpt.edu.vn>
2026-04-15 21:01:14 +0700

    update websocket

--- cosmate-backend/src/main/java/com/cosmate/configuration/WebSocketConfig.java
+++ cosmate-backend/src/main/java/com/cosmate/configuration/WebSocketConfig.java
@@ -25,6 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws-mobile").setAllowedOriginPatterns("*");
     }
 
     @Override
